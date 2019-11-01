4789094449d16c12aa584012e343b610be7f9c5e
Sergio Botero <sergiobuj@gmail.com>
2019-11-01 10:14:57 -0500

    fix init.js errors

--- init.js
+++ init.js
@@ -1,7 +1,24 @@
-config = {
+// The user initialization script allows you to run a custom JavaScript
+// script as AWS Cloud9 loads.
+
+// Warning: Adding code to this initialization script is currently an
+// experimental feature and is not fully supported. If you add
+// code to this script, you do so at your own risk. We reserve the
+// right to change this functionality at any time.
+// Note that using this initialization script to load third-party plugins by
+// using the internal loadPackage API is currently not officially
+// supported. If you attempt to load third-party plugins into the AWS Cloud9
+// IDE, you do so at your own risk. Third-party plugins can have full access
+// to all of your AWS Cloud9 environments, including their source code, AWS
+// credentials if present, and various other settings and files. We cannot
+// provide support for using third-party plugins, and we reserve the right to
+// disable this functionality at any time.
+
+/*global services*/
+({
     workDir: function (self) { return '/home/ubuntu/environment'; },
     repoURL: function (self) { return 'git@github.com:sergiobuj/c9-fiesta.git'; },
-    start: function () {
+    start: function (self) {
         services.proc.execFile('git',
             {
                 args: ['clone', self.repoURL(), self.workDir()],
@@ -15,6 +32,4 @@ config = {
                 args: ['./bootstrap.sh'], cwd: self.workDir()
             });
     },
-}
-
-config.start();
+}).start();
